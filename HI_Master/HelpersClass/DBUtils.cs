5d3945cdc7f06c12896734b0b9f75f607bd5d122
Stanislav Tolokolnikov <stanislav.tolokolnikov@gmail.com>
2022-07-27 12:38:06 +0300

    Created DBUtils class

--- HI_Master/HelpersClass/DBUtils.cs
+++ HI_Master/HelpersClass/DBUtils.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace HI_Master
 {
-    internal class DBUtils
+    class DBUtils
     {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "192.168.1.157";
+            int port = 3306;
+            string database = "handisitedb";
+            string username = "root";
+            string password = "root";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
     }
 }
