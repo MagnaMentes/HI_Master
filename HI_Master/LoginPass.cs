294a8af64f544191397ec88ac75f197544c45db9
StanislavTOLOKOLNIKO <StanislavTOLOKOLNIKO@JRJ04G3>
2022-07-27 11:19:25 +0300

    Add project files.

---
+++ HI_Master/LoginPass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HI_Master
+{
+    public partial class LoginPass : Form
+    {
+        public LoginPass()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
